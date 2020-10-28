68148b9611fd639ad48f7f5eef371c4bea61c276
Musa-1995 <72025497+Musa-1995@users.noreply.github.com>
2020-10-28 11:56:01 +0000

    creating objects

---
+++ objects.js
@@ -0,0 +1,9 @@
+var person1 = {
+    firstName:"Musa", lastName:"Umar", age: 20, eyeColor:"black"
+};
+var person2 = new Object();
+person2.firstName = "Umar";
+person2.lastName = "Anas";
+person2.age = "22";
+person2.eyeColor = "blue";
+console.log(person1,person2);
